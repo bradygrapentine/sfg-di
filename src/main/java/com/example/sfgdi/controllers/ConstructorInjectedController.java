d50501714e9cc05e4832c07973bcec42caaf8d92
bradygrapentine <brady.grapentine@gmail.com>
2021-10-15 11:07:36 -0400

    used annotations to tell spring to inject instance of greeting service

--- src/main/java/com/example/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,14 @@
 package com.example.sfgdi.controllers;
 
 import com.example.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
+    //Autowired not required
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
